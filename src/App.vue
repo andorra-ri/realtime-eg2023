0c8904184db10e5c117962593bff79c37d5ba39d
Marc Vilella <mvilella.and@gmail.com>
2023-03-15 01:16:05 +0100

    feat: Add base layout and header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
+  <header class="container">
+    <h1>{{ message('title') }}<em>{{ message('subtitle') }}</em></h1>
+    <p class="note">{{ message('last_update', { time: formatDate(lastUpdate) }) }}</p>
+  </header>
 </template>
 
 <script setup lang="ts">
+import { useI10n, useResults } from '/@/composables';
+
+const { message, formatDate } = useI10n();
+const { lastUpdate } = useResults();
 </script>
